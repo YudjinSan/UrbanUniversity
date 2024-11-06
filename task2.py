118d4911632919f5da405f4b749abea0bea11035
YudjinSan <gorlishef322@gmail.com>
2024-11-06 23:21:37 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,2 +1,5 @@
-#2nd program
-print(9.99 > 9.98 and 1000 != 1000.1)
+the_number_of_completed_DZ = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent / the_number_of_completed_DZ
+print ('Курс:',course_name,','' всего задач:',the_number_of_completed_DZ, ',' ' затрачено часов', number_of_hours_spent,  ',' ' среднее время выполнения', time_for_one_task)
